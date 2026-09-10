65ee667a6a926ea73db58407ba46208e0decab04
Vinay Goud <vinaygoud08@users.noreply.github.com>
2026-09-10 19:06:21 +0530

    fix(prod): output directly to root dist and set outputDirectory to dist for Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,6 +13,8 @@ export default defineConfig(({ command }) => ({
     ...(command === 'serve' ? [basicSsl()] : [])
   ],
   build: {
+    outDir: '../dist',
+    emptyOutDir: true,
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
       output: {
